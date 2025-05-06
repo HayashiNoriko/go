90a1f141ff0228a2912f92cda059d387cab63e1a
tinaxyfeng <tinaxyfeng@tencent.com>
2025-05-06 10:42:01 +0800

    零内存对象和接口的一些测试代码

---
+++ chapter03/01-零内存对象.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main1() {
+	var s1 struct{}
+	var s2 [0]int
+	var s3 [100]struct{}
+	var s4 = make([]struct{}, 100)
+
+	// 以下 4 行都是零内存对象【指针地址相同】
+	fmt.Printf("%p\n", &s1)
+	fmt.Printf("%p\n", &s2)
+	fmt.Printf("%p\n", &s3[66]) // 取任意元素
+	fmt.Printf("%p\n", &s4[66]) // 取任意元素
+
+	// 空切片 => 已初始化，但为空。语义上表示“有，但空”，行为更接近常规切片
+	// 【有自己独立的指针地址】
+	s5 := []int{}
+	fmt.Printf("%p\n", &s5)
+	fmt.Printf("%t\n", s5 == nil) // false
+
+	// nil 切片 => 未初始化，底层指针为 0。语义上类似“无”的，但可安全调用部分操作（如 append）
+	// 【有自己独立的指针地址】
+	// append 后，就不为 nil 了
+	var s6 []int
+	fmt.Printf("%p\n", &s6)
+	fmt.Printf("%t\n", s6 == nil) // true
+	s6 = append(s6, 1)
+	fmt.Printf("%t\n", s6 == nil) // false
+
+	// 空接口/任意类型
+	// 【有自己独立的指针地址】
+	var s7 any
+	fmt.Printf("%p\n", &s7)
+	fmt.Printf("%t\n", s6 == nil) // false
+
+}
