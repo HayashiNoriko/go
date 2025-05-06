b52f73fb83bcf7105f2826c3e37bdb1dd2867c59
tinaxyfeng <tinaxyfeng@tencent.com>
2025-05-06 17:19:54 +0800

    添加注释笔记

--- chapter05/worker_pool.go
+++ chapter05/worker_pool.go
@@ -30,6 +30,8 @@ func NewWorkerManager(nworkers int) *WorkerManager {
 func (wm *WorkerManager) StartWorkerPool() {
 	// 创建一定数量的 Worker
 	for i := 0; i < wm.nWorkers; i++ {
+		// 如果不使用 i := i，所有 goroutine 可能会看到相同的最终 i 值（通常是循环结束时的值）
+		// 在 Go 1.22 及以上版本中，这种行为已经改变，循环变量在每次迭代中会自动创建新实例，不再需要这种写法
 		i := i
 		wk := &Worker{id: i}
 		go wk.work(wm.workerChan)
@@ -62,11 +64,27 @@ type Worker struct {
 	err error
 }
 
+// 参数解释：
+//
+//	类型 chan<- *Worker 是一个只发送通道，表示这个函数只能向通道发送数据，不能从通道接收数据
+//	而 chan *Worker 是一个双向通道
+//	这样做明确函数意图、提高代码安全性
+//
+// 返回值解释：
+//
+//	err error，命名返回值。Go ​自动创建了一个名为 err 的变量，并初始化为该类型的零值​（对于 error 类型就是 nil）
+//	这相当于隐式执行了 var err error（初始值为 nil）
 func (wk *Worker) work(workerChan chan<- *Worker) (err error) {
 	// 任何 goroutine 只要异常退出或者正常退出都会调用 defer，所以在 defer 中向 WorkerManager 的 workerChan 发送通知
 	defer func() {
 		// 捕获异常信息，防止 panic直接退出
+		// recover() 是 Go 中用于捕获 panic 的内置函数
+		// 当 goroutine 发生 panic 时，recover() 可以捕获 panic 的值
+		// 只有在 defer 函数中调用 recover() 才有效
+		// 这里使用它是为了防止 worker goroutine 因 panic 而完全退出，而是将错误信息记录下来并通知主 goroutine
 		if r := recover(); r != nil {
+			// 如果 panic 的是 error 类型，直接保存
+			// 如果是其他类型，转换为 error 保存
 			if err, ok := r.(error); ok {
 				wk.err = err
 			} else {
