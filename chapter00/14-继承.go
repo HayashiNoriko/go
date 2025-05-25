62f0e54bc38d181d2af16f1acfeade9a646b4cae
tinaxyfeng <tinaxyfeng@tencent.com>
2025-05-25 20:25:50 +0800

    继承、接口断言

--- chapter00/14-继承.go
+++ chapter00/14-继承.go
@@ -2,11 +2,6 @@ package main
 
 import "fmt"
 
-// 接口
-type IBroker interface {
-	GetPendingTasks()
-}
-
 // 父类
 type Broker struct {
 	pendingTasks []string
@@ -23,15 +18,13 @@ type RedisBroker struct {
 	// queue string // 如果有，会覆盖父类字段；如果没有，会继承父类字段（rb.queue == rb.Broker.queue）
 }
 
+// 子类方法，如果有，会覆盖父类方法；如果没有，会继承父类方法（rb.GetPendingTasks == rb.Broker.GetPendingTasks）
 func (rb *RedisBroker) GetPendingTasks() {
-	fmt.Println("queue=", rb.queue)
-	fmt.Println("Broker.queue=", rb.Broker.queue)
-	fmt.Println("Get pending tasks from Redis")
-	fmt.Println(rb.Broker.pendingTasks)
+	fmt.Println("Redis broker pending tasks:", rb.pendingTasks)
 }
 
 func main14() {
-	var broker IBroker = &RedisBroker{
+	broker := &RedisBroker{
 		Broker: Broker{
 			pendingTasks: []string{"task1", "task2"},
 			queue:        "base tasks",
@@ -39,18 +32,10 @@ func main14() {
 		//queue: "derived tasks",
 	}
 
-	// 调用子类方法，成功
+	// 调用子类方法（如果子类实现了这个方法，那么就会调用子类的版本；如果没有实现，那么就会调用父类的版本）
 	broker.GetPendingTasks()
 
-	// 直接调用父类方法和字段，失败
-	// 因为 broker 是 IBrouker 接口类型，不是 RedisBroker 对象，还不具有 Broker 字段和 queue 字段
-	// broker.Broker.GetPendingTasks()    // 报错
-	// fmt.Println("queue=",broker.queue) // 报错
-
-	// 使用类型断言，把接口转换成子类对象，再调用父类方法和字段
-	if rb, ok := broker.(*RedisBroker); ok {
-		rb.Broker.GetPendingTasks()
-		fmt.Println("Broker.queue=", rb.queue)
-	}
+	// 显示调用父类方法
+	broker.Broker.GetPendingTasks()
 
 }
---
+++ chapter00/15-接口断言.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type IBook interface {
+	GetName() string
+}
+
+type Book struct {
+	Name string
+}
+
+func (b *Book) GetName() string {
+	return b.Name
+}
+
+func main() {
+	var mybook IBook = &Book{Name: "三国演义"}
+
+	// 两种获取书名的办法
+
+	// 1. 使用接口给我们提供的 GetName 方法
+	name := mybook.GetName()
+	fmt.Println(name)
+
+	// 2. 使用接口断言
+	if mb, ok := mybook.(*Book); ok {
+		fmt.Println(mb.Name)
+	}
+}
